f89907eeea8d927abfd77e581d65e7eb17481717
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2016-09-11 13:36:22 +0200

    Changes

--- app/src/main/java/pl/nowakowski_arkadiusz/chain_panic_button/activities/StartActivity.java
+++ app/src/main/java/pl/nowakowski_arkadiusz/chain_panic_button/activities/StartActivity.java
@@ -1,8 +1,10 @@
 package pl.nowakowski_arkadiusz.chain_panic_button.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import pl.nowakowski_arkadiusz.chain_panic_button.R;
 
@@ -18,5 +20,16 @@ public class StartActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_start, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.manage_chain:
+                //Intent intent = new Intent(this, AlarmChainActivity.class);
+                //startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
