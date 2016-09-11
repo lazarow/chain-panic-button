05c0c7e584e6c1a183c500650d5624845cc79e30
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2016-09-11 14:22:14 +0200

    - new activities - icons

--- app/src/main/java/pl/nowakowski_arkadiusz/chain_panic_button/activities/AlarmChainActivity.java
+++ app/src/main/java/pl/nowakowski_arkadiusz/chain_panic_button/activities/AlarmChainActivity.java
@@ -1,33 +1,36 @@
 package pl.nowakowski_arkadiusz.chain_panic_button.activities;
 
-import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import pl.nowakowski_arkadiusz.chain_panic_button.R;
 
-public class StartActivity extends AppCompatActivity {
+public class AlarmChainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.alarm_chain);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_alarm_chain);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_start, menu);
+        getMenuInflater().inflate(R.menu.menu_alarm_chain, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.manage_chain:
-                Intent intent = new Intent(this, AlarmChainActivity.class);
-                startActivity(intent);
+            case android.R.id.home:
+                finish();
                 break;
         }
         return super.onOptionsItemSelected(item);
