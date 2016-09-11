817c6741fc566204c3295e64776ff9fd30d679ea
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2016-09-12 00:45:23 +0200

    -

--- app/src/main/java/pl/nowakowski_arkadiusz/chain_panic_button/activities/StartActivity.java
+++ app/src/main/java/pl/nowakowski_arkadiusz/chain_panic_button/activities/StartActivity.java
@@ -21,29 +21,20 @@ import pl.nowakowski_arkadiusz.chain_panic_button.models.ChainLink;
  */
 public class StartActivity extends AppCompatActivity {
 
-    private TextView timer;
-    private long startTime = 0L;
-    private long limit = 10000; // The time span before sending notifications
-    private Handler handler = new Handler();
-    private long timeInMilliseconds = 0L;
-    private long timeSwapBuff = 0L;
-    private long updatedTime = 0L;
     private List<ChainLink> chainLinks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        timer = (TextView) findViewById(R.id.timer);
-        startTime = SystemClock.uptimeMillis();
+    }
+
+    @Override
+    protected void onResume() {
         ChainLinkDAO dao = new ChainLinkDAO(this);
-        List<ChainLink> chainLinks = dao.findAll();
+        chainLinks = dao.findAll();
         dao.close();
-        if (chainLinks.size() == 0) {
-            Toast.makeText(this, R.string.no_chain_links, Toast.LENGTH_SHORT).show();
-        } else {
-            handler.postDelayed(updateTimerThread, 0);
-        }
+        super.onResume();
     }
 
     @Override
@@ -62,21 +53,4 @@ public class StartActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    private Runnable updateTimerThread = new Runnable() {
-        public void run() {
-            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
-            updatedTime = Math.max(0, limit - (timeSwapBuff + timeInMilliseconds));
-            int seconds = (int) (updatedTime / 1000);
-            int minutes = seconds / 60;
-            seconds = seconds % 60;
-            int milliseconds = (int) (updatedTime % 1000);
-            timer.setText("" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds)
-                    + ":" + String.format("%02d", milliseconds));
-            handler.postDelayed(this, 0);
-            if (updatedTime == 0) {
-                handler.removeCallbacks(updateTimerThread);
-            }
-        }
-    };
 }
