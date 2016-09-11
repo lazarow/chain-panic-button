4032f9d600ea567b2c6b001e0669cec2db8b0edd
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2016-09-11 13:02:26 +0200

    A few activities

---
+++ app/src/main/java/pl/nowakowski_arkadiusz/chain_panic_button/activities/ChainActivity.java
@@ -0,0 +1,15 @@
+package pl.nowakowski_arkadiusz.chain_panic_button.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.nowakowski_arkadiusz.chain_panic_button.R;
+
+public class ChainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chain);
+    }
+}
